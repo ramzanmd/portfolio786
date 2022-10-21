55102634a7081db8721a84313befae9304aca8b7
Md Ramzan <ramzanmd865@gmail.com>
2022-10-21 22:44:09 +0530

    third commit

--- mywesite/settings.py
+++ mywesite/settings.py
@@ -20,7 +20,7 @@ BASE_DIR = Path(__file__).resolve().parent.parent
 # See https://docs.djangoproject.com/en/4.1/howto/deployment/checklist/
 
 # SECURITY WARNING: keep the secret key used in production secret!
-SECRET_KEY = ['django-insecure-n0@8zst@)4%vva(tom!^*14a-iz0!^-(z4mv^y9lq*6#-wyw5z']
+SECRET_KEY = ['SECRET_KEY']
 
 # SECURITY WARNING: don't run with debug turned on in production!
 DEBUG = False
